db9fad9f24af2b1f64b273958e602070524664ae
hkacmaz <halilkacmaz@outlook.com>
2019-07-30 11:40:39 -0700

    EntryClass where customer can have methods to access their account via relavent methods

---
+++ src/Phase1/Account.java
@@ -0,0 +1,13 @@
+package Phase1;
+
+import java.math.BigInteger;
+
+public class Account {
+	
+	int account_number = 123456789;
+	double balance = 50.0;
+	String customer_name = "Halil";
+	String email = "asdf@asdf.com";
+	int phone_number = 415770;
+
+}
