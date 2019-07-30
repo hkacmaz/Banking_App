73b241248acdd80b0b033697c125b02d42960c43
hkacmaz <halilkacmaz@outlook.com>
2019-07-30 12:10:13 -0700

    Account constructor has been created

--- src/Phase1/Account.java
+++ src/Phase1/Account.java
@@ -10,6 +10,14 @@ public class Account {
 	String email = "asdf@asdf.com";
 	int phone_number = 415770;
 	
-	
+	public void Account() {
+		
+		System.out.println("Account No: " + account_number);
+		System.out.println("Current Balance: "+ balance);
+		System.out.println("Account Holder: "+ customer_name);
+		System.out.println("Email: "+ email);
+		System.out.println("Phone Number: "+ phone_number);
+		
+	}
 
 }
